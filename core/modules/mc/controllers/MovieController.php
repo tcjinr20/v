fbff644651ff59ad161e2a08f68232968a50832a
polly <tcjinr20@163.com>
2018-04-09 17:43:21 +0800

    change

---
+++ core/modules/mc/controllers/MovieController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/9 0009
+ * Time: 16:24
+ */
+
+namespace app\modules\mc\controllers;
+
+
+use app\modules\mc\models\VideoForm;
+
+class MovieController extends Controller
+{
+    public function actionIndex(){
+        $id=\Yii::$app->request->get('id');
+        if(empty($id)){
+            throw new \Exception('wrong');
+        }
+        $list=VideoForm::randomVideo();
+        $v=new VideoForm();
+        $v->video_id=$id;
+        $v->store_id=$this->store_id;
+        $b=$v->getMovie();
+//        var_dump($b);
+        return $this->render('index',['list'=>$list,'movie'=>$b]);
+    }
+}
